7445db950f56890586db6b6e7ffdaf608627ba83
thegithubgeek <wfrankw9@gmail.com>
2017-06-14 21:18:53 -0700

    :)(

--- src/com/github/thegithubgeek/AI2048/Main.java
+++ src/com/github/thegithubgeek/AI2048/Main.java
@@ -2,14 +2,20 @@ package com.github.thegithubgeek.AI2048;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+
+import com.github.thegithubgeek.AI2048.Player.ArrayIndexComparator;
+
 import java.util.*;
 public class Main {
 	static int NUM_PLAYERS = 1000;
 	static int KILL_RATE = 500;
+	static int NUM_TRIAL = 5;
+	static int GEN_NUM = 100;
 	static Player[] players = new Player[NUM_PLAYERS];
-	static int[] scores = new int[NUM_PLAYERS];
+	static Integer[] scores = new Integer[NUM_PLAYERS];
 	static boolean lost = false;
 	static Game2048 game2048;
+	static int player;
 	public static void main(String[] args) {
 		JFrame game = new JFrame();
 		game.setTitle("2048 Game");
@@ -25,30 +31,57 @@ public class Main {
 	    for(int i=0; i<NUM_PLAYERS; i++){
 	    	players[i]=Player.genRanPlayer();
 	    }
-	    try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {}
-	    generation();
-	    Arrays.sort(scores);
-	    int max = scores[NUM_PLAYERS-1];
-	    System.out.println(max);
+	    for(int i=0; i<GEN_NUM; i++){
+		    try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {}
+		    generation();
+		    System.out.println("GEN: "+GEN_NUM);
+	    }
+	    run(players[player], 100);
 	}
 	public static void generation(){
 		for (int i=0; i<players.length; i++) {
-			Player player = players[i];
-			run(player);
-			System.out.println(i);
-			scores[i] = Game2048.myScore;
-			game2048.resetGame();
-			lost = false;
+			scores[i] = 0;
+			for (int j = 0; j < NUM_TRIAL; j++) {
+				Player player = players[i];
+				run(player, 0);
+				scores[i] += Game2048.myScore;
+				game2048.resetGame();
+				lost = false;
+			}
+			scores[i]/=NUM_TRIAL;
+			if(i%100==0){
+				System.out.println(i);
+			}
+		}
+	    ArrayIndexComparator<Integer> comparator = new ArrayIndexComparator<Integer>(scores);
+		Integer[] indexes = comparator.createIndexArray();
+		Arrays.sort(indexes, comparator);
+		Arrays.sort(scores);
+	    int max = scores[NUM_PLAYERS-1];
+	    System.out.println("Score: "+max);
+	    player = indexes[NUM_PLAYERS-1];
+	    System.out.println("Player: "+player);
+	    int sum = 0;
+        for(int i=0; i < scores.length ; i++)
+                sum = sum + scores[i];
+        double average = sum / scores.length;
+        System.out.println(average);
+	    nextGen();
+	}
+	//kill off the bad players and reproduce
+	public static void nextGen(){
+		for (int i = 0; i < KILL_RATE; i++) {
+			players[i] = players[i+KILL_RATE].mutate();
 		}
 	}
-	public static void run(Player p){
+	public static void run(Player p, int delay){
 		while(!lost){
 			p.move();
-//			try {
-//				Thread.sleep(1);
-//			} catch (InterruptedException e) {}
+			try {
+				Thread.sleep(delay);
+			} catch (InterruptedException e) {}
 		}
 	}
 	public static void getScore() {
--- src/com/github/thegithubgeek/AI2048/Player.java
+++ src/com/github/thegithubgeek/AI2048/Player.java
@@ -1,10 +1,11 @@
 package com.github.thegithubgeek.AI2048;
 import java.util.*;
-import java.util.stream.IntStream;
 
 import com.github.thegithubgeek.AI2048.Game2048.Tile;
 public class Player {
 	static int MAX_INIT_LAYER_NUM = 2;
+	static float MAX_CHANGE = 0.05f;
+	static float DIR_CHANGE_PROB = 0.01f;
 	ArrayList<Float> weights = new ArrayList<>();
 	ArrayList<Integer> dir = new ArrayList<>();
 	static int index = 0;
@@ -26,11 +27,9 @@ public class Player {
 	}
 	
 	public void move() {
-		ArrayList<Float> board = new ArrayList<>();
 		Float[] vote = {0f,0f,0f,0f};
 		for (int i = 0; i < 16; i++) {
-			board.add(weights.get(i)*Game2048.getTile(i));
-			vote[dir.get(i)] += board.get(i);
+			vote[dir.get(i)] += weights.get(i)*Game2048.getTile(i);
 		}
 		
 		ArrayIndexComparator<Float> comparator = new ArrayIndexComparator<Float>(vote);
@@ -46,7 +45,22 @@ public class Player {
 			Main.lost=true;
 		}
 	}
-	class ArrayIndexComparator<T extends Comparable<T>> implements Comparator<Integer>
+	
+	public Player mutate(){
+		ArrayList<Float> newWeights = new ArrayList<>();
+		ArrayList<Integer> newDir = new ArrayList<>();
+		for (int i = 0; i < 16; i++) {
+			newWeights.add((float) (weights.get(i)+(Math.random()-0.5)*2*MAX_CHANGE+weights.get(i)));
+			if(Math.random()<=DIR_CHANGE_PROB){
+				newDir.add((int) (Math.random()*4));
+			}else{
+				newDir.add(dir.get(i));
+			}
+		}
+		return new Player(newWeights, newDir);
+	}
+	
+	static class ArrayIndexComparator<T extends Comparable<T>> implements Comparator<Integer>
 	{
 	    private final T[] array;
 
